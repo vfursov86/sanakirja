074a1cbbe72c3b25b7b5f67ea5f534abd3b49cc6
vfursov86 <vfursov86@gmail.com>
2024-11-15 20:00:06 +0300

    Final

--- obj/Debug/net8.0/sanakirja.AssemblyInfo.cs
+++ obj/Debug/net8.0/sanakirja.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sanakirja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25e3367c6167ec2a21d47e2a772478b900a64006")]
 [assembly: System.Reflection.AssemblyProductAttribute("sanakirja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sanakirja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
