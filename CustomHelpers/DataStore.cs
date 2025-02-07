cda252bc0f0473eb09b254deab5dceb7140086bf
vfursov86 <vfursov86@gmail.com>
2025-02-07 11:52:25 +0300

    Copilot improvements.

--- CustomHelpers/DataStore.cs
+++ CustomHelpers/DataStore.cs
@@ -9,66 +9,34 @@ public static class DataStore
     public static SortedDictionary<string, string> sanastoData = new();
     // Stores words for Pikkusanat page.
     public static SortedDictionary<string, string> pikkusanatData = new();
-    // Keeps data for Fraaseja page.
+    // Keeps data for Fraaseja page
     public static SortedDictionary<string, string> fraasejaData = new();
     public static void LoadData()
     {
         using (var connection = new SqliteConnection("Data Source=sanakirjadata.db"))
         {
             connection.Open();
-            var command = connection.CreateCommand();
 
-            // Load from finnruswords table and set them to _sanastoData dictionary.
-            command.CommandText = @"
-                                    select id, finnish, russian
-                                    from finnruswords
-                                ";
+            LoadTableData(connection, "finnruswords", sanastoData);
+            LoadTableData(connection, "pikkusanat", pikkusanatData);
+            LoadTableData(connection, "fraaseja", fraasejaData);
+        }    
+    }
 
-            using (var reader = command.ExecuteReader())
+    private static void LoadTableData(SqliteConnection connection, string tableName, SortedDictionary<string, string> dataDictionary)
+    {
+        var command = connection.CreateCommand();
+        command.CommandText = $@"select id, finnish, russian from {tableName}";
+
+        using (var reader = command.ExecuteReader())
+        {
+            while (reader.Read())
             {
-                while (reader.Read())
-                {
-                    var id = reader.GetString(0);
-                    var finn = reader.GetString(1);
-                    var rus = reader.GetString(2);
-                    
-                    sanastoData.Add(finn, rus);
-                }
+                var finn = reader.GetString(1);
+                var rus = reader.GetString(2);
+
+                dataDictionary.Add(finn, rus);
             }
-            // Load from pikkusanat table and set them to _pikkusanatData dictionary.
-            command.CommandText = @"
-                                    select id, finnish, russian
-                                    from pikkusanat
-                                ";
-
-            using (var reader = command.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    var id = reader.GetString(0);
-                    var finn = reader.GetString(1);
-                    var rus = reader.GetString(2);
-                    
-                    pikkusanatData.Add(finn, rus);
-                }
-            }
-
-            command.CommandText = @"
-                                    select id, finnish, russian
-                                    from fraaseja
-                                ";
-            // Load from fraaseja table and set them to _fraasejaData dictionary.
-            using (var reader = command.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    var id = reader.GetString(0);
-                    var finn = reader.GetString(1);
-                    var rus = reader.GetString(2);
-
-                    fraasejaData.Add(finn, rus);
-                }
-            }                   
         }
     }
 }
