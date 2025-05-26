3a0c55c6c6f76c90a75c9066225798280a735c9a
vfursov86 <vfursov86@gmail.com>
2025-05-26 21:18:59 +0300

    Data populated.

--- obj/Debug/net8.0/sanakirja.AssemblyInfo.cs
+++ obj/Debug/net8.0/sanakirja.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sanakirja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+340872015989abe4719f503dc71420eb4acb75c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2ca35eeff2448443f5091fdda4beef9e9abfcd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("sanakirja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sanakirja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
