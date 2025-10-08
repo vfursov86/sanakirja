39e77cadf4631fd09612db4d9e1b9bb5980761df
vfursov86 <vfursov86@gmail.com>
2025-10-08 08:02:12 +0300

    New words!

--- obj/Debug/net8.0/sanakirja.AssemblyInfo.cs
+++ obj/Debug/net8.0/sanakirja.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sanakirja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f1a5a1643dc53c9a07d073382ce14f0a0751ce6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f12e16dd1db06aa7c03576562389d993b5534177")]
 [assembly: System.Reflection.AssemblyProductAttribute("sanakirja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sanakirja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
