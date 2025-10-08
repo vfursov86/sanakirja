985c2e464fe97a7b6de3186d81fda38191c1c1ba
vfursov86 <vfursov86@gmail.com>
2025-10-08 22:45:18 +0300

    Search added, Verbi Rektio added, some design improvements.

--- obj/Debug/net8.0/sanakirja.AssemblyInfo.cs
+++ obj/Debug/net8.0/sanakirja.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sanakirja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98af0f26d6e620561ab7f913f3ebf90eb2436444")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5c4707585c1c4ae2c6811e11654b9902576d89a")]
 [assembly: System.Reflection.AssemblyProductAttribute("sanakirja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sanakirja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
